b2eb2bb82a05d647213dcd641b09602fbcaa59dc
reshadsaidov <alhadss@code.edu.az>
2023-08-24 16:45:54 +0400

    com

---
+++ EmailValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    public class EmailValidator
+    {
+
+
+            
+        
+
+
+        public bool Validate(string email)
+        {
+
+
+            var pattern = @"@mail.com$";
+
+            var regex = new Regex(pattern);
+             
+
+            
+            if (email.Length < 10)
+            {
+                throw new Exception("ShortMailException");
+            }
+            if (regex.IsMatch(email)==false)
+            {
+                throw new Exception("NotAnEmailAddressException");
+
+            }
+
+            return true;
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp3
+{
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailValidator emailValidator = new EmailValidator();
+
+            try
+            {
+                bool IsValid = emailValidator.Validate("brock@mail.com");
+                Console.WriteLine(IsValid);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
